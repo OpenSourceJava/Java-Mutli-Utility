b914736a6821f2d641cb84cea86604d99d821e9f
Zachery Kish <openSourceJava@hotmail.com>
2014-01-21 22:15:22 -0600

    Update ClientSide.java

--- common/org/phin/muc/network/ClientSide.java
+++ common/org/phin/muc/network/ClientSide.java
@@ -3,7 +3,8 @@ package org.phin.muc.network;
 public class ClientSide {
 	
 	public static void clientInit() {
-		
+		// will be implemented in the future 
+		// and once the srver sid eis actually created :)
 	}
 
 }
